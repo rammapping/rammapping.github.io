1114b03e8bb8c3dbe83e64aaf6f19bbd7c7c037b
rammapping <rammapping@gmail.com>
2021-08-17 11:10:23 -0400

    Update

---
+++ HPMD/index.js
@@ -0,0 +1,35 @@
+function setBasemap (basemap) {
+    if (layer) {
+      map.removeLayer(layer);
+    }
+
+    if (basemap === "OpenStreetMap") {
+        layer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        })
+    } else {
+        layer = L.esri.basemapLayer(basemap);
+    }
+
+//    layer = L.esri.basemapLayer(basemap);
+
+    map.addLayer(layer);
+
+    // Label Controls
+    if (layerLabels) {
+      map.removeLayer(layerLabels);
+    }
+
+    if (
+      basemap === 'ShadedRelief'
+    ) {
+        layerLabels = L.esri.basemapLayer(basemap + 'Labels');
+        map.addLayer(layerLabels);
+    } else if (basemap.includes('Imagery')) {
+          layerLabels = L.esri.basemapLayer('ImageryLabels');
+          map.addLayer(layerLabels);
+    } else if (basemap.includes("OpenStreetMap")) {
+        false
+    }
+
+    }
