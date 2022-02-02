64cb208a8a68112deb6a3b8df9506571fddbdcba
rammapping <rammapping@gmail.com>
2022-02-02 11:43:48 -0500

    feat: adding many new pages

---
+++ HPMD/lineChart.js
@@ -0,0 +1,60 @@
+const colorOptions = ["#6aa84f", "#3d85c6", "#e69138", "#674ea7", "#a64d79"]
+
+function assembleDataObj(labels, labelText, graphData, border) {
+    let data = {
+      labels: labels,
+      datasets: [{
+        label: labelText,
+        data: graphData,
+        fill: false,
+        // borderColor: 'rgb(75, 192, 192)',
+        borderColor: border,
+        tension: 0.1
+      }]
+    };
+    return data
+}
+
+
+function createLineConfig(data, titleText) {
+    let config = {
+          type: 'line',
+          data: data,
+          options: {
+              responsive: true,
+              layout: {
+                padding: 50
+                },
+              interaction: {
+                intersect: false,
+                mode: 'index',
+                },
+              plugins: {
+                    legend: {
+                            display: false,
+                    },
+                    title: {
+                        display: true,
+                        text: titleText
+                    }
+                },
+              scales: {
+                x: {
+                  display: true,
+                  title: {
+                    display: true,
+                    text: 'Season Day'
+                  }
+                },
+                y: {
+                  display: true,
+                  title: {
+                    display: true,
+                    text: 'Harvests'
+                  }
+                }
+              },
+            }            
+        };
+        return config
+}
